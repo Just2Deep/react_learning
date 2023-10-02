889adc3625039a5f3746e2330b40feb4878a4552
Just2Deep <deepakrajsrs@gmail.com>
2023-10-02 12:19:20 +0530

    sample component creation

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,5 +1,12 @@
+import Chai from "./Chai";
+
 function App() {
-  return <h1>Hi Mom!</h1>;
+  return (
+    <>
+      <Chai />
+      <h1>Hi Mom!</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,7 @@
+const Chai = () => {
+  return <h2>Chai is Ready</h2>;
+};
+
+// When creating components, functions name should be Capitalised along with filenames
+
+export default Chai;
