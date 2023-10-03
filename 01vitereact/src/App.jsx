9d229639775b4809931e4cafaf396847a20fc9cf
Just2Deep <deepakrajsrs@gmail.com>
2023-10-03 19:40:20 +0530

    how react works behind the scenes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,14 @@
 import Chai from "./chai";
+
 function App() {
-  return <Chai />;
+  const adjective = "fun";
+  return (
+    <>
+      <Chai />
+      {/* You can only add evaluated expressions as props */}
+      <h1>react is {adjective}</h1>
+    </>
+  );
 }
 
 export default App;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+const anotherUser = "some other user";
 const anotherElement = (
   <a href="https://google.com" target="_blank">
     visit google
@@ -14,7 +15,8 @@ const reactElement = React.createElement(
     href: "https://google.com",
     target: "_blank",
   },
-  "click me to visit google!"
+  "click me to visit google! ",
+  anotherUser
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
