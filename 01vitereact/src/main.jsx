eb5fb188eb9672b6785646fe858f7d2eba19fb79
Just2Deep <deepakrajsrs@gmail.com>
2023-10-03 19:18:37 +0530

    create a reactElement

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    visit google
+  </a>
 );
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "click me to visit google!"
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
