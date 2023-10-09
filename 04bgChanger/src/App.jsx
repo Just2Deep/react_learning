9ce7930bb23a07b86b32b141d781c3b41b7790a6
Just2Deep <deepakrajsrs@gmail.com>
2023-10-09 20:57:11 +0530

    Add text to show color selected

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [color, setColor] = useState("olive");
+  const [color, setColor] = useState("white");
 
   return (
     <div
-      className="w-full h-screen duration-200"
+      className="w-full h-screen duration-200 flex justify-center items-center"
       style={{ backgroundColor: color }}
     >
+      <p className="text-5xl text-white">
+        You Chose {color[0].toUpperCase() + color.slice(1)}
+      </p>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
